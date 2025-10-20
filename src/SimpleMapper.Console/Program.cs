cb5b55bed7a5dd794e139d3d075eee254132c14f
Ryszard Balcerzak <rysz.b82@gmail.com>
2025-10-20 16:54:16 +0200

    Add code to the test client

--- src/SimpleMapper.Console/Program.cs
+++ src/SimpleMapper.Console/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SimpleMapper.Core;
+
+var mapper = new Mapper(new MappingRegistry(new MappingBuilder()));
+
+var source = new Person { Name = "Alice", Age = 30 };
+var destination = mapper.Map<Person, PersonDto>(source);
+
+Console.WriteLine(destination.Name); // Alice
+Console.WriteLine(destination.Age);  // 30
+
+internal class PersonDto
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+internal class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
